3b34f2fe5b8d269f1bba8d15215da0c2a8f8e324
Szymczok <filipszymala@gmail.com>
2026-04-08 22:38:01 +0200

    Add BoardTuple class for better tuple representation and prettier printing of game board

--- util/files.py
+++ util/files.py
@@ -1,3 +1,27 @@
+from typing import override
+
+
+class BoardTuple:
+    def __init__(self, board: list, rows: int, cols: int):
+        self.board = tuple(board)
+        self.rows = rows
+        self.cols = cols
+        self.zero_idx = self.board.index(0)
+
+    @override
+    def __str__(self):
+        result = ""
+        for i in range(len(self.board)):
+            if self.board[i] / 10 >= 1:
+                result += f"{self.board[i]} "
+            else:
+                result += f"{self.board[i]}  "
+
+            if (i + 1) % self.cols == 0:
+                result += "\n"
+        return result
+
+
 def load_input(input_file_path: str):
     with open(input_file_path, 'r') as file:
         data = file.readlines()
@@ -9,7 +33,8 @@ def load_input(input_file_path: str):
     for row in data[1:]:
         flat_board.extend(map(int, row.split()))
 
-    return tuple(flat_board), rows, cols
+    return BoardTuple(flat_board, rows, cols), rows, cols
+
 
 def save_solution(solution: list, solution_details: list, solution_file_path: str, solution_details_file_path: str):
     with open(solution_file_path, 'w') as file:
