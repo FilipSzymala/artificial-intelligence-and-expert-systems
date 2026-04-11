3ef4c5fb56df4d954d7048c0991bd2346f3e0ed9
Szymczok <filipszymala@gmail.com>
2026-04-11 15:44:49 +0200

    add comments

--- util/algoritms/bfs.py
+++ util/algoritms/bfs.py
@@ -1,8 +1,12 @@
 from collections import deque
-from util.nodes import is_goal
+from util.nodes import is_goal, get_neighbors
 
-def bfs(G, S, dbs):
+# S - start node
+# dbs - desired board state
+def bfs(S, cols, rows, search_neighbors_strategy, dbs):
+    # kolejka stanow otwartych
     Q = deque()
+    # zbior stanow zamknietych
     T = set()
     Q.append(S)
 
@@ -11,7 +15,7 @@ def bfs(G, S, dbs):
         if is_goal(v, dbs):
             return True
         T.add(v)
-        for i in G.neighbors(v):
+        for i in get_neighbors(v, cols, rows, search_neighbors_strategy):
             if i not in T and i not in Q:
                 Q.append(i)
     return False
