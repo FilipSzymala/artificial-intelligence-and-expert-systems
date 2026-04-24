4c372f0c0c9ea00c996f7b8be9e455bddb1687bc
Szymczok <filipszymala@gmail.com>
2026-04-24 17:23:30 +0200

    Clean ready to mark branch to maximize script simplicity

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from util.parser import *
 
-from util.program_modes import verify_all_mode, test_all_mode, default_mode, generate_charts_mode, \
-    test_number_of_fails_mode
+from util.program_modes import default_mode
 
 if __name__ == '__main__':
     args = parse_args()
     show_args(args)
 
-    if args.mode == "test_all":
-        test_all_mode()
-    elif args.mode == "verify_all":
-        verify_all_mode()
-    elif args.mode == "generate_charts":
-        generate_charts_mode()
-    elif args.mode == "test_number_of_fails":
-        test_number_of_fails_mode()
-    else:
-        default_mode(args)
-
+    default_mode(args)
